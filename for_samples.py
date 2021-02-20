eb8c59f806c91b7475b3129a76d117a01ff8cbd3
kostya <mcantoharip@gmail.com>
2021-02-20 17:59:02 +0300

    nothing

--- for_samples.py
+++ for_samples.py
@@ -20,5 +20,18 @@ def training(prof=''):
     return render_template('training.html', **param)
 
 
+@app.route('/list_prof', defaults={'list_prof': ''})
+@app.route('/list_prof/<list>')
+def list(list_str):
+    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                   'инженер по терраформированию', 'климатолог',
+                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                   'штурман', 'пилот дронов']
+    param = {}
+    param['list_prof'] = list_prof
+    return render_template('list_prof.html')
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
