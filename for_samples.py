97a88fd9f0630e3a7a4c16dec797b7694ec4abdb
kostya <mcantoharip@gmail.com>
2021-02-27 17:09:52 +0300

    keep writing

--- for_samples.py
+++ for_samples.py
@@ -1,36 +1,65 @@
-from flask import Flask, url_for, render_template
-import json
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/<title>')
-@app.route('/index/<title>')
-def index(title):
-    param = {}
-    param['title'] = title
-    return render_template('base.html', **param)
+@app.route('/')
+def nothing():
+    return 'Миссия Колонизация Марса'
 
 
-@app.route('/training/<prof>')
-@app.route('/training', defaults={'prof': ''})
-def training(prof=''):
-    param = {}
-    param['prof'] = prof.lower()
-    return render_template('training.html', **param)
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
 
 
-@app.route('/list_prof', defaults={'list_prof': ''})
-@app.route('/list_prof/<list>')
-def list(list_str):
-    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
-                   'инженер по терраформированию', 'климатолог',
-                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
-                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
-                   'штурман', 'пилот дронов']
-    param = {}
-    param['list_prof'] = list_prof
-    return render_template('list_prof.html')
+@app.route('/promotion')
+def promotion():
+    output = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+              'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!',
+              'Присоединяйся!']
+    return '</br>'.join(output)
+
+
+@app.route('/image_mars')
+def image_mars():
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet"  
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Жди нас, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='/img/mars.png')}">
+                        <h2>Вот она какая, красная планета.</h2>
+                      </body>
+                    </html>'''
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='/css/style.css')}" />
+                            <title>Жди нас, Марс!</title>
+                          </head>
+                          <body>
+                            <h1>Жди нас, Марс!</h1>
+                            <img src="{url_for('static', filename='/img/mars.png')}">
+                            <h2>Человечество вырастает из детства.</h2>
+                            <h3>Человечеству мала одна планета.</h3>
+                            <h4>Мы сделаем обитаемыми безжизненные пока планеты.</h4>
+                            <h5>И начнем с Марса!</h5>
+                            <h6>Присоединяйся!</h6>
+                          </body>
+                        </html>'''
 
 
 if __name__ == '__main__':
